a32376326de04bb425e6e3235002fb020ed6cd18
Rachael Ainsworth <rainsworth@users.noreply.github.com>
2018-11-30 13:19:49 +0000

    Update eMERLIN_CP_IRIS_test.sh
    fixed singularity binding issues

--- eMERLIN_CP_IRIS_test/eMERLIN_CP_IRIS_test.sh
+++ eMERLIN_CP_IRIS_test/eMERLIN_CP_IRIS_test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 /bin/hostname
 /bin/date
 /bin/ls -la
@@ -29,14 +29,16 @@ which python
 tar -xvzf 3C277.1_eMERLIN.tar.gz
 mv eMERLIN_CASA_Pipeline_clean.ipynb data/
 mv inputs.txt data/
+tar -xvzf CASA_eMERLIN_pipeline.tar.gz
+mv CASA_eMERLIN_pipeline pipeline
 
-git clone https://github.com/e-merlin/eMERLIN_CASA_pipeline.git
+#git clone https://github.com/e-merlin/eMERLIN_CASA_pipeline.git
 
-singularity pull --name jupyter-casa.simg shub://aardk/jupyter-casa:docker
+#singularity pull --name jupyter-casa.simg shub://aardk/jupyter-casa:docker
 
 echo "printenv in the container: "
-singularity exec --cleanenv -H $PWD -B data/:$HOME/data -B eMERLIN_CASA_pipeline:$HOME/pipeline jupyter-casa.simg printenv
+singularity exec --cleanenv -H $PWD -B data/:$PWD/data -B pipeline/:$PWD/pipeline jupyter-casa.simg printenv
 
-singularity exec --cleanenv -H $PWD -B data/:$HOME/data -B eMERLIN_CASA_pipeline:$HOME/pipeline jupyter-casa.simg './runjupyter_eMERLIN_CP.sh'
+singularity exec --cleanenv -H $PWD -B data/:$PWD/data -B pipeline/:$PWD/pipeline jupyter-casa.simg './runjupyter_eMERLIN_CP.sh'
 
 tar -cvf eMERLIN_CP_IRIS_test_output_$3.tar data
